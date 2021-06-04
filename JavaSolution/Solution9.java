354d2fe8003919e9c8a1b0cf91799ad63fe2b0f8
hardiknasare <nasare.hardik@gmail.com>
2021-06-04 11:05:15 +0530

    HackerRank Java Solution

---
+++ JavaSolution/Solution9.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution9 {
+
+    public static void main(String[] args) {
+        String s = null;
+        int i = 1;
+        Scanner input = new Scanner(System.in);
+       
+        while(input.hasNext())
+        {
+            s=input.nextLine();
+        System.out.println(i+ " "+s);
+        i++;
+        }
+    }
+}
